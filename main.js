d831a8296a5d9b38c44a11ff79935d5df9a50872
qqwqq97 <99179348+qqwqq97@users.noreply.github.com>
2023-03-09 16:36:41 +0900

    JS : home transparent

--- main.js
+++ main.js
@@ -35,6 +35,11 @@ function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: 'smooth'});
 }
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
 
+document.addEventListener('scroll' , () => {
+    home.style.opacity =  1 - window.scrollY / homeHeight;
+})
 
 
